973d0782e678b84643a922cf87b0d146a6e2784b
高云 <1609810432@qq.com>
2021-05-31 23:38:18 +0800

    commit tabbar first time

---
+++ src/components/tabbar/TabBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="tabbar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+  components: {}
+};
+</script>
+
+<style scoped>
+#tabbar {
+  display: flex;
+  position: fixed;
+  right: 0;
+  left: 0;
+  bottom: 0;
+
+  background-color: #f6f6f6;
+  box-shadow: 0 -3px 1px rgba(100, 100, 100, 0.1);
+}
+</style>
---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="tab-bar-item">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :class="{ active: isActive }"><slot name="item-text"></slot></div>
+  </div>
+  <!-- <div class="tab-bar-item">分类</div>
+      <div class="tab-bar-item">购物车</div>
+      <div class="tab-bar-item">我的</div> -->
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  components: {},
+  data() {
+    return {
+      isActive: true
+    };
+  }
+};
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  height: 49px;
+  text-align: center;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  height: 24px;
+  width: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+}
+.active {
+  color: red;
+}
+</style>
